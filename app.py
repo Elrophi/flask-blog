abbae81e85e97feaad29f1359f01ad95aab40b6c
el <elrophilskwaila@gmail.com>
2021-04-14 13:46:32 +0300

    opened connection to the db file

--- app.py
+++ app.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
